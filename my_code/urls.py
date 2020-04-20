ed5aa4582d7ad981afecb0dd66afda1c3808aacf
DebjyotiRay <33850567+DebjyotiRay@users.noreply.github.com>
2020-04-20 08:54:22 +0530

    First commit

---
+++ my_code/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from app import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls'))
+
+
+]
